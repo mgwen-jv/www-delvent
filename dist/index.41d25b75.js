0ec132021295069b79e59fcf222e7e27f63b084a
Sara <audupeace862@gmail.com>
2024-09-29 19:32:21 +0100

    Updated json

---
+++ dist/index.41d25b75.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("signupForm");
+    const formContainer = document.getElementById("formContainer");
+    const notificationCard = document.getElementById("notificationCard");
+    if (form) form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        const email = document.getElementById("email").value;
+        console.log("Form submitted:", {
+            firstName,
+            lastName,
+            email
+        });
+        if (formContainer) formContainer.style.display = "none";
+        // Show the notification card
+        if (notificationCard) notificationCard.style.display = "block";
+        form.reset();
+    });
+    const closeNotification = document.getElementById("closeNotification");
+    if (closeNotification) closeNotification.addEventListener("click", function() {
+        notificationCard.style.display = "none";
+        formContainer.style.display = "block";
+    });
+});
+
+//# sourceMappingURL=index.41d25b75.js.map
